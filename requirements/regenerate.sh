2b9913d0b760ecb49d76cd31e4666509e0319b43
Tom Cobb <tom.cobb@diamond.ac.uk>
2024-01-18 17:09:37 +0000

    r

--- requirements/regenerate.sh
+++ requirements/regenerate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CONSTRAINTS=$(dirname $0)/requirements/dev-constraints.txt
+CONSTRAINTS=$(dirname $0)/dev-constraints.txt
 if [ "$1" == "-f" ] || [ -s $CONSTRAINTS ]; then
     pip freeze --exclude-editable > $CONSTRAINTS
 fi
